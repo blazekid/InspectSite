14938751dfe667c67a12501380afdf8ed90bc798
sandeep <sk3.sandeep7@gmail.com>
2021-12-07 00:04:13 +0530

    Updated the static files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'blog/index.html', {})
+
+def pricing(request):
+    return render(request, 'blog/pricing.html', {})
+
+def team(request):
+    return render(request, 'blog/team.html', {})
